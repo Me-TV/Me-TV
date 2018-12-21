d9e641b4c42190397ae732ca653d3aa1804e84d0
Russel Winder <russel@winder.org.uk>
2018-12-21 15:01:57 +0000

    Hack up a Meson build to install the new support executables.

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cargo build --manifest-path $1/Cargo.toml --release && cp $1/target/release/me-tv $2
+cargo build --manifest-path $1/Cargo.toml --release && cp $1/target/release/me-tv $2 && cp $1/target/release/me-tv-record $1/target/release/me-tv-schedule $(dirname $2)
