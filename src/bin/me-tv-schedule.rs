151fc6d12ef899f48ecf63cf631e82405398fd81
Russel Winder <russel@winder.org.uk>
2018-12-22 06:30:27 +0000

    Update the CLI handling. Add adapter and frontend specification for recording.

--- src/bin/me-tv-schedule.rs
+++ src/bin/me-tv-schedule.rs
@@ -45,60 +45,66 @@ fn main() {
         .author("Russel Winder <russel@winder.org.uk>")
         .about("Schedule recording to create an MPEG4 file.
 
-A channel name, a start time, and either an end time or a duration
-must be provided.
+A channel name, a start time, a file path, and either an end time
+or a duration must be provided.
 ")
         .arg(Arg::with_name("adapter")
-             .short("a")
-             .long("adapter")
-             .value_name("NUMBER")
-             .help("Sets the adapter number to use, default 0")
-             .takes_value(true))
+            .short("a")
+            .long("adapter")
+            .value_name("NUMBER")
+            .help("Sets the adapter number to use, default 0.")
+            .takes_value(true)
+            .default_value("0"))
         .arg(Arg::with_name("frontend")
-             .short("f")
-             .long("frontend")
-             .value_name("NUMBER")
-             .help("Sets the frontend number to use, default 0")
-             .takes_value(true))
+            .short("f")
+            .long("frontend")
+            .value_name("NUMBER")
+            .help("Sets the frontend number to use, default 0.")
+            .takes_value(true)
+            .default_value("0"))
         .arg(Arg::with_name("channel")
-             .short("c")
-             .long("channel")
-             .value_name("CHANNEL")
-             .help("Sets the channel name, no default")
-             .takes_value(true)
-             .required(true))
+            .short("c")
+            .long("channel")
+            .value_name("CHANNEL")
+            .help("Sets the channel name, no default.")
+            .takes_value(true)
+            .required(true))
         .arg(Arg::with_name("start_time")
-             .short("s")
-             .long("start-time")
-             .value_name("TIME")
-             .help("Sets the start time of recording, ISO8601 format, no default")
-             .takes_value(true)
-             .required(true))
+            .short("s")
+            .long("start-time")
+            .value_name("TIME")
+            .help("Sets the start time of recording, ISO8601 format, no default.")
+            .takes_value(true)
+            .required(true))
         .arg(Arg::with_name("end_time")
-             .short("e")
-             .long("end-time")
-             .value_name("TIME")
-             .help("Sets the end time of recording, ISO8601 format, no default")
-             .takes_value(true))
+            .short("e")
+            .long("end-time")
+            .value_name("TIME")
+            .help("Sets the end time of recording, ISO8601 format, no default. This must be set if duration is not, but do not set both.")
+            .takes_value(true)
+            .conflicts_with("duration"))
         .arg(Arg::with_name("duration")
-             .short("d")
-             .long("duration")
-             .value_name("TIME")
-             .help("Sets the duration of recording in minutes, no default")
-             .takes_value(true))
+            .short("d")
+            .long("duration")
+            .value_name("TIME")
+            .help("Sets the duration of recording in minutes, no default. This must be set unless end-time is, but do not set both.")
+            .takes_value(true)
+            .required_unless("end-time"))
         .arg(Arg::with_name("output")
-             .short("o")
-             .long("output")
-             .value_name("PATH")
-             .help("Path to output file.")
-             .takes_value(true)
-             .required(true))
+            .short("o")
+            .long("output")
+            .value_name("PATH")
+            .help("Path to output file, no default.")
+            .takes_value(true)
+            .required(true))
         .arg(Arg::with_name("verbose")
             .short("v")
             .long("verbose")
             .help("sets verbose mode"))
         .get_matches();
     let be_verbose = matches.is_present("verbose");
+    let adapter = matches.value_of("adapter").unwrap().parse::<u8>().expect("Couldn't parse adapter value as a positive integer.");
+    let frontend = matches.value_of("frontend").unwrap().parse::<u8>().expect("Couldn't parse frontend value as a positive integer.");
     let channel = matches.value_of("channel").unwrap();
     let start_time = convert_datetime(iso8601::datetime(matches.value_of("start_time").unwrap()).expect("Could not parse start time."));
     let end_time = match matches.value_of("end_time") {
@@ -126,10 +132,25 @@ must be provided.
     };
     let output_file = matches.value_of("output").unwrap();
     if be_verbose {
-        println!("Scheduling recording of channel '{}' at {:?} for {} minutes to file {}.", channel, start_time, duration.num_minutes(), output_file);
+        println!(
+            "Scheduling recording of channel '{}' at {:?} for {} minutes to file {} using adapter {}, frontend {}.",
+            channel,
+            start_time,
+            duration.num_minutes(),
+            output_file,
+            adapter,
+            frontend,
+        );
     }
     let echo_process = process::Command::new("echo")
-        .arg(format!("me-tv-record --channel={} --duration={} --output={}", channel, duration.num_minutes(), output_file))
+        .arg(format!(
+            "me-tv-record --channel={} --duration={} --output={} --adapter={} --frontend={}",
+            channel,
+            duration.num_minutes(),
+            output_file,
+            adapter,
+            frontend,
+        ))
         .stdout(process::Stdio::piped())
         .spawn()
         .expect("Failed to start echo process.");
